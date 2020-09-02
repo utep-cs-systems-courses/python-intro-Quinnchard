23fc1770f73c114e5f2bd3c7b74def1ad7c7f04c
Quinnchard <rquinn2@miners.utep.edu>
2020-09-02 11:56:24 -0600

    Running into issue where not all arguments are converted to a string

---
+++ wordCount.py
@@ -0,0 +1,22 @@
+import sys
+import string
+
+inputfname = sys.argv[1]
+outputfname = sys.argv[2]
+
+with open(inputfname, 'r') as inputFile:
+    d = dict()
+    for line in inputFile:
+        line = line.strip()
+        line = line.lower()
+        line = line.translate(line.maketrans("", "", string.punctuation))
+        words = line.split(" ")
+        for word in words:
+            if word in d:
+                d[word] = d[word] + 1 
+            else:
+                d[word] = 1 
+                
+with open(outputfname, 'w') as outputFile:
+    for key in list(d.keys()):
+        outputFile.write('%s:%s\n' % (key, ":", d[key]))
